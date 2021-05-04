df9be502f4fa2c37ce888d7a333a592b82aaa26f
Michael Lobov <michael.lobov13@gmail.com>
2021-05-04 19:55:21 +0300

    2. Каркас шахматной доски [#515146]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -5,30 +5,47 @@ import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
 public class BishopBlack implements Figure {
-    private final Cell position;
+	private final Cell position;
 
-    public BishopBlack(final Cell ps) {
-        position = ps;
-    }
+	public BishopBlack(final Cell ps) {
+		position = ps;
+	}
 
-    @Override
-    public Cell position() {
-        return position;
-    }
+	@Override
+	public Cell position() {
+		return position;
+	}
 
-    @Override
-    public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
-    }
+	@Override
+	public Cell[] way(Cell dest) {
+		if (!isDiagonal(position, dest)) {
+			throw new ImpossibleMoveException(
+				String.format("Could not move by diagonal from %s to %s", position, dest)
+			);
+		}
+		int size = Math.abs(dest.getX() - position.getX());
+		System.out.println(size);
+		Cell[] steps = new Cell[size];
+		int deltaX = (dest.getX() - position.getX()) / size;
+		int deltaY = (dest.getY() - position.getY()) / size;
+		for (int index = 0; index < size; index++) {
+			int x = position.getX() + ((index + 1) * deltaX);
+			int y = position.getY() + ((index + 1) * deltaY);
+			steps[index] = Cell.findBy(x, y);
+		}
+		return steps;
+	}
 
-    public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
-    }
+	public boolean isDiagonal(Cell source, Cell dest) {
+		if (Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    @Override
-    public Figure copy(Cell dest) {
-        return new BishopBlack(dest);
-    }
+	@Override
+	public Figure copy(Cell dest) {
+		return new BishopBlack(dest);
+	}
 }
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Test;
+import ru.job4j.chess.FigureNotFoundException;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.Logic;
+import ru.job4j.chess.OccupiedCellException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class BishopBlackTest {
+
+	@Test
+	public void position() {
+		BishopBlack bishopBlack = new BishopBlack(Cell.A1);
+		Cell expected = Cell.A1;
+		assertThat(expected, is(bishopBlack.position()));
+	}
+
+	@Test
+	public void copy() {
+		BishopBlack bishopBlack = new BishopBlack(Cell.A1);
+		Figure figure = bishopBlack.copy(Cell.B3);
+		Cell expected = Cell.B3;
+		assertThat(expected, is(figure.position()));
+	}
+
+	@Test
+	public void way() {
+		BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+		Cell[] result = bishopBlack.way(Cell.G5);
+		Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+		assertThat(expected, is(result));
+	}
+
+	@Test(expected = ImpossibleMoveException.class)
+	public void diagonalFalse() {
+		BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+		bishopBlack.way(Cell.G6);
+	}
+}
